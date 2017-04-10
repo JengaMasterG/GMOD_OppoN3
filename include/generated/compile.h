a13ba93c94a9df2db4d3c86e9049ae132a73da34
ZeroX25 <grsruggirello25@gmail.com>
2017-04-10 15:43:08 -0400

    First commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sun Apr 9 13:51:26 EDT 2017"
+#define LINUX_COMPILE_BY "giovanni"
+#define LINUX_COMPILE_HOST "giovanni-desktop"
+#define LINUX_COMPILER "gcc version 4.8 (GCC) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.4.0-g3c7a335-dirty"
